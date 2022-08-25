6527f692f41f159261442e4d1013e22c641d7dfc
TzMusic <74445170+TzMusic@users.noreply.github.com>
2022-08-25 23:21:03 +0600

    Update config.js

--- config.js
+++ config.js
@@ -6,22 +6,22 @@ module.exports = {
     url_link: 'https://eager-wozniak-e3574e.netlify.app',
 
     discord: {
-        client_id: '730219913951707186',
-        client_secret: 'ZoYIxhxC2_tpzyL9RRYM_n55QfOs1ty6',
-        token: 'NzMwMjE5OTEzOTUxNzA3MTg2.XwadIA.bXkU8bMRLifczmfy6nHvVM2dTVA',
+        client_id: '1012408271656714361',
+        client_secret: '5rStIED70m-cGe5nnyJMsOlxsALmRKh9',
+        token: 'MTAxMjQwODI3MTY1NjcxNDM2MQ.GFfCZj.ETDIJI-pDVK1keO0zFXh0aQhDoVUuqy4ltueP4',
         notification: true,
-        guild_id: '730063771778678829',
-        mod_role_id: '730223401607888986',
-        admin_role_id: '730226711823646770',
-        BOT_ROLE_ID: '730578235209023688',
-        UNVERIFIED_ROLE_ID: '730655812606885970',
-        ADMIN_USER: '332109764286742529',
-        log_id: '730230291687276604',
-        mod_log_id: '730223337967452230',
-        VERIFY_ROLES_ID: '730749423876964454',
-        UNMUTED_ROLES_ID: '730749679016607815',
-        BOT_DEV_ROLE: '730709935851307049',
-        GUILD_INVITE: 'https://discord.gg/5pVRxjR'
+        guild_id: '643682896569303050',
+        mod_role_id: '1010743570589822996',
+        admin_role_id: '1010743570589822996',
+        BOT_ROLE_ID: '1010743570589822996',
+        UNVERIFIED_ROLE_ID: '1010743570589822996',
+        ADMIN_USER: '1010743570589822996',
+        log_id: '1010743603913576577',
+        mod_log_id: '1010743603913576577',
+        VERIFY_ROLES_ID: '1010743570589822996',
+        UNMUTED_ROLES_ID: '1010743570589822996',
+        BOT_DEV_ROLE: '1010743570589822996',
+        GUILD_INVITE: 'https://discord.gg/vtRcuUrpW9'
     },
 
     database: {
